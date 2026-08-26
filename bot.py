22e29643a48624b7e1959d29daeb7c5b5abcdd4f
akifbaba6363-hub <akifbaba6363@gmail.com>
2026-08-26 22:48:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,21 @@
 import os
+from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+load_dotenv()
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Selam reis! Bot online 🔥")
+    await update.message.reply_text("Bot çalışıyor reis! /start ✅")
 
 def main():
-    app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
     print("Bot başlıyor...")
+    app = ApplicationBuilder().token(TOKEN).build()
+    
+    app.add_handler(CommandHandler("start", start))
+    
+    print("Bot aktif. Telegram'dan /start at")
     app.run_polling()
 
 if __name__ == "__main__":
