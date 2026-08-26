f447373e03b7f05aaca9e7407baad8906fe22032
akifbaba6363-hub <akifbaba6363@gmail.com>
2026-08-26 21:30:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +7,5 @@ def anasayfa():
     return "Bot calisiyor reis!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
